d784f1c25cb9252c2be5760d4cc5357587174d0b
jaggesher <jaggesher14@gmail.com>
2017-04-08 20:42:55 +0600

    All COmplete Without Optimal

--- OS LAB/BestFit.cpp
+++ OS LAB/BestFit.cpp
@@ -1,1 +1,90 @@
+/*
+    Name:
+    ID:
+    TITLE: Best Fit Algorithm.
+
+*/
+
+#include<bits/stdc++.h>
+#define pf printf
+#define sf scanf
+#define PB push_back
+using namespace std;
+
+class Block{
+    public:
+        int ID;
+        int Size;
+        bool isFree;
+};
+
+int FreeMemory,MemBlock,ProcessNo;
+vector<Block>BlockInfo;
+vector<int>Process;
+bool compare(const Block &ob1,const Block &ob2){
+    if(ob1.Size<ob2.Size) return true;
+    return false;
+}
+void Input(){
+    Block ob;
+    int tm;
+    pf("Enter The Number Of Memory Block:");
+    sf("%d",&MemBlock);
+    for(int i=1;i<=MemBlock;i++){
+            pf("Enter The Size Of BLOCK%d: ",i);
+        sf("%d",&tm);
+        ob.ID=i;
+        ob.Size=tm;
+        ob.isFree=true;
+        FreeMemory+=tm;
+        BlockInfo.PB(ob);
+    }
+    pf("\nEnter The Number Of Process:");
+    sf("%d",&ProcessNo);
+
+    for(int i=1;i<=ProcessNo;i++){
+        pf("Enter The Size Of PROCESS%d: ",i);
+        sf("%d",&tm);
+        Process.PB(tm);
+    }
+}
+
+void Output(){
+    pf("\n\n FileNO  |  FileSize  |  BlockNo   |   BlockSize  |  InternalFragmentation\n");
+    pf(" -------------------------------------------------------------------------\n");
+    for(int i=0;i<ProcessNo;i++){
+        bool flag=true;
+        for(int j=0;j<MemBlock;j++)
+            if(BlockInfo[j].Size>=Process[i] && BlockInfo[j].isFree){
+                int temp=BlockInfo[j].Size-Process[i];
+                printf("  %3d    |    %3d     |    %3d     |     %3d      |         %3d        \n",(i+1),Process[i],BlockInfo[j].ID,BlockInfo[j].Size,temp);
+                flag=false;
+                BlockInfo[j].isFree=false;
+                break;
+            }
+        if(flag) pf("  %3d    |    %3d     |     XXX    |     XXX      |         XXX        \n",(i+1),Process[i]);
+    }
+    pf(" -------------------------------------------------------------------------");
+    return;
+}
+
+int main(){
+    Input();
+    sort(BlockInfo.begin(),BlockInfo.end(),compare);
+    Output();
+    return 0;
+}
+/*
+SAMPLE INPUT:
+3
+300
+200
+400
+3
+100
+150
+250
+
+
+*/
 
--- OS LAB/FIFO.cpp
+++ OS LAB/FIFO.cpp
@@ -1,1 +1,82 @@
+/*
+    Name:
+    ID:
+    TITLE: FIFO Page Replacement Algorithm.
+
+*/
+
+#include<bits/stdc++.h>
+#define pf printf
+#define sf scanf
+#define PB push_back
+#define MAX 1000
+using namespace std;
+int No_Of_Pages,No_Of_Frame;
+int Pages[MAX],Frame[MAX],FrameInfo[MAX];
+
+int VictimFrame(){
+    int tm=FrameInfo[0];
+    int indx=0;
+    for(int i=1;i<No_Of_Frame;i++)
+    if(tm>FrameInfo[i]){
+        indx=i;
+        tm=FrameInfo[i];
+    }
+    return indx;
+}
+
+bool IsPageAlreadyIn(int tm){
+    for(int i=0;i<No_Of_Frame;i++) if(Frame[i]==tm) return true;
+    return false;
+}
+
+void Input(){
+
+    pf("Enter The Number Of Pages: ");
+    sf("%d",&No_Of_Pages);
+    pf("Enter Page Ref String: ");
+    for(int i=0;i<No_Of_Pages;i++)sf("%d",&Pages[i]);
+    pf("Enter The Number Of Frame: ");
+    sf("%d",&No_Of_Frame);
+}
+
+void Output(){
+    int Fault=0;
+    pf("\nAt The Beginning The Status Of Frames: ");
+    for(int i=0;i<No_Of_Frame;i++) {
+            pf("-1 ");
+            Frame[i]=FrameInfo[i]=-1;
+    }
+
+    pf("\n\nStatus Of FRAME In Main Memory: \n");
+    for(int i=0;i<No_Of_Pages;i++){
+        pf(" %2d :",i+1);
+        if(IsPageAlreadyIn(Pages[i])){
+             for(int i=0;i<No_Of_Frame;i++) pf("%2d ",Frame[i]);
+             pf("\n");
+        }else{
+            int tm=VictimFrame();
+            Frame[tm]=Pages[i];
+            FrameInfo[tm]=(i+1);
+            Fault++;
+            for(int i=0;i<No_Of_Frame;i++) pf("%2d ",Frame[i]);
+            pf("->Fault\n");
+        }
+    }
+
+    pf("\n\nTotal Number Of Page Fault: %d\n\n",Fault);
+    return;
+}
+
+int main(){
+    Input();
+    Output();
+    return 0;
+}
+/*
+SAMPLE INPUT:
+20
+7 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 7 0 1
+3
+*/
 
