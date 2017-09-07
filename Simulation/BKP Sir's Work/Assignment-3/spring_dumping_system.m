e17e2b70d18458c0ff3d29dd94e647ba95bd4c73
Aktarul Islam <aktarul857@gmail.com>
2017-09-07 14:07:29 +0600

    Assignment(1-3) completed

--- Simulation/BKP Sir's Work/Assignment-3/spring_dumping_system.m
+++ Simulation/BKP Sir's Work/Assignment-3/spring_dumping_system.m
@@ -1,18 +1,19 @@
 function spring_dumping_system()  
 clear all; close all;  
 
-time_span =0:0.001:10; %time in seconds.  
+time_span =0:0.001:18; %time in seconds.  
 k = 40; % spring stiffness. N/m 
 m = 5;  % mass, kg
 x0 = [0 0]; %initial_position & initial_speed
 P = 100;   % force amplitude 
 f=10;  %unit step 
-c=3.0; %damping coefficient 
+c=3; %damping coefficient 
  
 [t,x]=ode45(@myFunction,time_span,x0); 
-plot(t,x(:,1)); 
+plot(t,x(:,1),'g'); 
 ylim([-.1 .5]); 
 grid on;
+xlabel('time'); ylabel('Displacement');
 
     % solves m x''+ c x' + k x = f(t) 
     function xdot=myFunction(t,x) 
