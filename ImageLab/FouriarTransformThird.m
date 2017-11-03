8126dc2fc1ce10973ca46653c00f08fc0184e0e0
Jaggesher Mondal <jaggesher14@gmail.com>
2017-11-04 01:45:00 +0600

    Fouriar Transform Third Complete

--- ImageLab/FouriarTransformThird.m
+++ ImageLab/FouriarTransformThird.m
@@ -6,20 +6,31 @@ Noise_Img = imnoise(Img,'salt & pepper');
 
 %fft_Img=fft2(Noise_Img);
 
-fft_Img=fft2(Img);
+fft_Img=fft2(Noise_Img);
 fft_shift_Img= fftshift(fft_Img);
 
-[ir,ic] = size(Img); 
+ObservePSNR=zeros(1,29);
+
+D = 250; %Cut Off
+
+[ir,ic] = size(Noise_Img); 
 hr = (ir-1)/2; 
 hc = (ic-1)/2; 
 [x, y] = meshgrid(-hc:hc, -hr:hr);
-z = sqrt(x.^2+y.^2);
-
-c= z<25; %Cut Off 
-
-lpf= fft_shift_Img .* c;
-
-Re_Img= mat2gray(abs(ifft2(lpf)));
+for i=1:29
+   
+    c =1./(1+((x.^2+y.^2)/D)^(2*i));
+  
+    blpf= fft_shift_Img .* c;
+    Re_Img= mat2gray(abs(ifft2(blpf)));
+   
+    %PSNR CAL
+    maxValue = double(max(Noise_Img(:)));
+    mseImage = (double(Noise_Img) - double(Re_Img)) .^ 2;
+    [rows columns] = size(Noise_Img);   
+    mse = sum(mseImage(:)) / (rows * columns);
+    ObservePSNR(i) = 10 * log10( 256^2 / mse);
+end
 
 %Ploting Area.
 row=2;
@@ -33,19 +44,18 @@ imshow(Noise_Img);
 title('Noisy Image');
 
 subplot(row,col,3);
-imshow(mat2gray(log(1+abs(lpf))));
-title('Low pass Filter');
+imshow(mat2gray(log(1+abs(fft_shift_Img))));
+title('FFT Shift Image');
 
 subplot(row,col,4);
-imshow(Re_Img);
-title('Reconstruect Image');
-
-%subplot(row,col,3);
-%figure;
-%imshow(mat2gray(log(1+abs(fft_shift_Img))));
-%title('FFT Shift Image');
+plot(ObservePSNR);
+ylabel('PSNR')
+xlabel('Order Of filter')
+title('PSNR');
 
 
+
+ 
 
 
 
